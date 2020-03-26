e4bd09cac698cc59383e3195c56843c53af16ab6
Tolulope Wojuola <toluwojuola@gmail.com>
2020-03-26 23:49:47 +0100

    Add files via upload

---
+++ wojay1.js
@@ -0,0 +1,28 @@
+let name = "Wojuola Tolulope"
+
+let courses = ["HTML", " CSS", " Javascript", " Design"]
+
+console.log("My name is "+name)
+
+console.log("My courses are " +courses)
+
+
+const data = [];
+
+
+ for (let i=0; i<=200; i++) {
+
+   
+  if (i%2===0) {
+
+  
+    data.push(i);
+
+
+               }
+
+ 
+          }
+
+ 
+console.log(data);
